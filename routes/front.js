6e28d0f5bfdf485cdc3cb012d7ff4cd0f90e7c98
Razvan Goldstein <razvan.goldstein@gmail.com>
2018-04-18 22:28:52 +0300

    send host env variable to view

--- routes/front.js
+++ routes/front.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/main', (req, res) =>{
-    res.render('main')
+    res.render('main', {host: process.env.HOST})
 })
 
 module.exports = router
